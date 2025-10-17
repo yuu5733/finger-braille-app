c93f2f2388558deed508ef85ea40143ce530dc30
yuu5733 <you1yi1.st0ries+github@gmail.com>
2025-10-17 14:04:52 +0900

    仮置き（rafce）

---
+++ src/components/BrailleTable/BrailleRow.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BrailleRow = () => {
+  return (
+    <div>BrailleRow</div>
+  )
+}
+
+export default BrailleRow
---
+++ src/components/BrailleTable/BrailleTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BrailleTable = () => {
+  return (
+    <div>BrailleTable</div>
+  )
+}
+
+export default BrailleTable
